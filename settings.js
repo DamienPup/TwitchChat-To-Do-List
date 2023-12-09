59c7319bce2cdf85afb0319ae8454e69f90c0db9
DamienPup <s154095g@gmail.com>
2023-12-08 22:11:07 -0900

    Added new settings.
    TODO: Add implementation for the new settings.
    
    - Added a better way (settings entry instead of index.js mod) to change the names for commands. Spaces are now supported in command names and multiple names can map to the same command.
    - The format of the help output for the commands can now be changed (advanced).

--- settings.js
+++ settings.js
@@ -6,8 +6,52 @@ const config = (() => {
     const scrollPxGap = 0; // Gap between last and first list item. Any positive whole number.
     const scrollLoopDelaySec = 2.5; // Pause between loops in seconds. Any (positive) number.
 
-    // End of settings. DO NOT TOUCH THIS SECTION UNLESS UPDATING FROM A PREVIOUS VERSION.
+    // Names of different commands. Spaces allowed.
+    // You can add multiple names for a single command, or just have one
+    // The FIRST entry will be displayed in the help command as the primary command.
+    // The remaining entries will be displayed as alises.
+    const commandNames = {
+        // Add a task to the list
+        "add" : ["task add", "tasks:add"],
+        // Finish a task
+        "done" : ["task done", "tasks:done"],
+        // Remove a task
+        "remove" : ["task remove", "tasks:remove"],
+        // Edit a task
+        "edit" : ["task edit", "tasks:edit"],
+        // Clear all tasks or just finished ones
+        "clear" : ["task clear", "tasks:clear"],
+        // List all commands
+        "help" : ["task help", "tasks:help"],
+        // List bot credits
+        "credits" : ["task credits", "tasks:credits"],
+        // Reload bot and overlay.
+        "reload" : ["task reload", "tasks:reload"],
+    }
+    // FYI on format:
+    // The part to the left of the colon is the command to be run.
+    // The part to right is a list of possible names you can type in chat.
+    // E.g. if you set: "add" : ["task add", "tasks:add"],
+    // Then "!task add" OR "!tasks:add" will add a task to the last ("add" command)
+    
+    // !!! Advanced settings below !!!
+
+    // Feel free to change the names of the parameters.
+    // Just make sure it's still clear how many there are and what they are.
+    const commandSyntaxes = {
+        "add" : "<task>",
+        "done" : "<number>",
+        "remove" : "<number>",
+        "edit" : "<number> <new task>",
+        "clear" : "<done|all>",
+        "help" : "",
+        "credits" : "",
+        "reload" : "",
+    }
+
+    // !!! End of settings. DO NOT TOUCH THIS SECTION UNLESS UPDATING FROM A PREVIOUS VERSION.
     return {
-        taskLimit, scrollingEnabled, scrollPxPerSecond, scrollPxGap, scrollLoopDelaySec
+        taskLimit, scrollingEnabled, scrollPxPerSecond, scrollPxGap, scrollLoopDelaySec, commandNames,
+        commandSyntaxes
     };
 })();
